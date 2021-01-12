14e5b4063ebdc35658749e6f97a68425a4cff335
Ruben Matamoros <ruben@valiantcreative.net>
2021-01-12 17:53:13 -0500

    created icons,tests,and updated object methods and user prompts

--- src/page-template.js
+++ src/page-template.js
@@ -66,7 +66,7 @@ const generatePage = templateData => {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css" />
     </head>
     <body>
-        <section class="hero is-primary is-bold">
+        <section class="hero is-info is-bold">
             <div class="hero-body">
                 <div class="container">
                     <h1 class="title">My Team</h1>
